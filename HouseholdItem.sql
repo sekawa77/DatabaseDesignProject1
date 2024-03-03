d2c22bd84fdd27bf707e7727da0d6a869a91ef68
sekawa77 <55608166+sekawa77@users.noreply.github.com>
2024-03-03 02:52:55 -0500

    Update HouseholdItem.sql

--- HouseholdItem.sql
+++ HouseholdItem.sql
@@ -4,5 +4,5 @@ CREATE TABLE HouseholdItem (
     brand VARCHAR(255),
 	itemName VARCHAR(255),
 	FOREIGN KEY (householdItemTypeID) REFERENCES HouseholdItemType(householdItemTypeID) ON UPDATE CASCADE,
-	UNIQUE(householdItemID)
+	UNIQUE(householdItemID, itemName)
 );
