3e230764ea69eceb20fcde6d1c9373a43ecc2d59
sekawa77 <55608166+sekawa77@users.noreply.github.com>
2024-03-03 02:57:42 -0500

    Delete ItemListData.sql

--- ItemListData.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO ItemList (listID, userID, listName)
-VALUES 
-	(1, 1, "Medicine"),
-	(2, 1, "Foods"),
-	(3, 2, "Cosmetics"),
-	(4, 3, "Shopping List"),
-	(5, 4, "Snacks"),
-	(6, 5, "Ingredients");
